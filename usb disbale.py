6cfff41d0357483b2d7e757f223996553b265620
Sandip sahebrao Kulat <110375266+sandipkulat18@users.noreply.github.com>
2022-08-01 16:54:13 +0530

    Add files via upload

---
+++ usb disbale.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[8]:
+
+
+pd.read_csv ("file.tsv",sep="\t")
+
+
+# In[10]:
+
+
+pd.read_excel (r"C:\Users\sandip\Desktop\Usb Disbale List.xlsx")
+
+
+# In[ ]:
+
+
+
+
