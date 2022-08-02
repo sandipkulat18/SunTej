ec9df65be8eb2f0fc136decfa2acb40c6d000744
Sandip sahebrao Kulat <110375266+sandipkulat18@users.noreply.github.com>
2022-08-02 13:18:50 +0530

    Add files via upload

---
+++ addition decimal.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+first=input("enter first number :")
+second = input("enter second number :")
+sum= float(first) + float(second)
+print(sum)
+
+
+# In[ ]:
+
+
+
+
