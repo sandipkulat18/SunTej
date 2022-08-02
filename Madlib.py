c7f56c2cd372ba2ac85661db93f6034f55d5cbdb
Sandip sahebrao Kulat <110375266+sandipkulat18@users.noreply.github.com>
2022-08-02 12:43:24 +0530

    Add files via upload

---
+++ Madlib.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+colour=input ("enter your colour :")
+noun=input ("enter your noun :")
+celebrity=input ("enter your celebrity :")
+
+print("Roses are " + colour)
+print(noun + " are blue")
+print("I love " + celebrity)
+
+
+# In[ ]:
+
+
+
+
