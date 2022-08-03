0e6a3d513358702606fce4f9094e522666a78b8a
Sandip sahebrao Kulat <110375266+sandipkulat18@users.noreply.github.com>
2022-08-03 14:12:44 +0530

    Update Guess gaming.py

--- Guess gaming.py
+++ Guess gaming.py
@@ -2,10 +2,20 @@
 # coding: utf-8
 
 # In[11]:
+secert_word="sandip"
+guess =""
 
+guess_count=0
+guess_limit =3
 
-def cube(num):
-    return num*num*num
-
-result=cube(87)
-print(result)
+out_of_guessess= False
+while guess != secert_word and not(out_of_guessess):
+    if guess_count < guess_limit:
+        guess= input(" enter guess :")
+        guess_count =guess_count + 1
+    else:
+        out_of_guessess = True
+if out_of_guessess:
+    print("Out of Guesses, You Lose!!")
+else:
+    print("You Win !!")
