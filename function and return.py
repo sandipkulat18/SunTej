74ac4d644e39fed1cc8873b35b44d4f9d8c918df
Sandip sahebrao Kulat <110375266+sandipkulat18@users.noreply.github.com>
2022-08-03 12:34:24 +0530

    Add files via upload

---
+++ function and return.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[28]:
+
+
+def sayhi(name,age):
+    print("Hello " + name + ", you are " + age + ".")
+sayhi("sandip","28")
+sayhi("Tejsavi","25")
+
+
+# In[38]:
+
+
+def xyz(num):
+    return num*num*num
+xyz(5)
+
+
+# In[ ]:
+
+
+def xyz(num):
+    return num*num*num
+result=xyz(4)
+print(result)
+
+
+# In[ ]:
+
+
+
+
