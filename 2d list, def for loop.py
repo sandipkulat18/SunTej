8439fa6defb49a507d060657853f0b9990598c20
Sandip sahebrao Kulat <110375266+sandipkulat18@users.noreply.github.com>
2022-08-03 14:49:15 +0530

    Add files via upload

---
+++ 2d list, def for loop.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[29]:
+
+
+same=[(1,2,3),
+     (2,3,4),
+     (2,3,4)]
+for number in same:
+    for row in number:
+        print(row)
+
+
+# In[24]:
+
+
+def min(num1,num2,num3):
+    if num1 <= num2 and num1 <= num1:
+        return num1
+    elif num2 <=num1 and num2 <=num3:
+        return num2
+    else: 
+        return num3
+print(min(4,300,40))
+    
+
+
+# In[26]:
+
+
+def max(num1,num2,num3):
+    if num1 >= num2 and num1 >= num1:
+        return num1
+    elif num2 >=num1 and num2 >=num3:
+        return num2
+    else: 
+        return num3
+print(max(4,300,40))
+    
+
+
+# In[21]:
+
+
+for i in range(5):
+    if i ==0:
+        print("first interation")
+    else:
+        print ("Not first")
+        
+        
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
